61e6ce1a8ffcbcbd2e62f2532ca75a7869ee9335
Chin <chin.bimbo@gmail.com>
2013-01-24 00:40:39 -0500

    change to beautiful layout
    slow as hell in VS (Metro mode) but runs normal in browser

---
+++ VietnameseLunarCalendar/js/data.js
@@ -0,0 +1,14 @@
+var codropsEvents = {
+	//'11-23-2012' : '<a href="http://tympanus.net/codrops/2012/11/23/three-script-updates/">Three Script Updates</a>',
+	//'11-21-2012' : '<a href="http://tympanus.net/codrops/2012/11/21/adaptive-thumbnail-pile-effect-with-automatic-grouping/">Adaptive Thumbnail Pile Effect with Automatic Grouping</a>',
+	//'11-20-2012' : '<a href="http://tympanus.net/codrops/2012/11/20/learning-principles-for-improving-your-css/">Learning Principles for Improving Your CSS</a>',
+	//'11-19-2012' : '<a href="http://tympanus.net/codrops/2012/11/19/responsive-css-timeline-with-3d-effect/">Responsive CSS Timeline with 3D Effect</a>',
+	//'11-14-2012' : '<a href="http://tympanus.net/codrops/2012/11/14/creative-css-loading-animations/">Creative CSS Loading Animations</a>',
+	//'11-13-2012' : '<a href="http://tympanus.net/codrops/2012/11/13/baraja-a-plugin-for-spreading-items-in-a-card-like-fashion/">Baraja: A Plugin for Spreading Items in a Card-Like Fashion</a>',
+	//'11-12-2012' : '<a href="http://tympanus.net/codrops/2012/11/12/mobile-design-typography-is-vitally-important-and-challenging/">Mobile Design Typography is Vitally Important … and Challenging</a>',
+	//'11-09-2012' : '<a href="http://tympanus.net/codrops/2012/11/09/responsive-wordpress-theme-giveaway/">Responsive WordPress Theme Giveaway</a>',
+	//'11-06-2012' : '<a href="http://tympanus.net/codrops/2012/11/06/gamma-gallery-a-responsive-image-gallery-experiment/">Gamma Gallery: A Responsive Image Gallery Experiment</a>',
+	//'11-02-2012' : '<a href="http://tympanus.net/codrops/2012/11/02/heading-set-styling-with-css/">Heading Set Styling with CSS</a>',
+	//'12-25-2012' : '<span>Christmas Day</span>',
+	//'12-31-2012' : '<span>New Year\'s Eve</span>'
+};
